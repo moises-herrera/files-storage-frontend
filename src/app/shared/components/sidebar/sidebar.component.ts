a17034715d9dd6a0ca11469a5a2a4785e7509bef
moises-herrera <moisesherrera037@gmail.com>
2025-03-25 11:21:21 -0500

    feat: logout user

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MenuItem } from '../../models/menu-item';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { AlertService } from 'src/app/core/services/alert.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,14 +13,42 @@ import { ButtonModule } from 'primeng/button';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  private readonly userService = inject(UserService);
+
+  private readonly router = inject(Router);
+
+  private readonly alertService = inject(AlertService);
+
   readonly links: MenuItem[] = [
     {
-      title: 'Home',
+      title: 'Inicio',
       url: '/home',
+      icon: 'pi pi-home',
     },
     {
-      title: 'Almacenamiento',
+      title: 'Archivos',
       url: '/storage',
+      icon: 'pi pi-folder',
+    },
+    {
+      title: 'Configuración',
+      url: '/settings',
+      icon: 'pi pi-cog',
     },
   ];
+
+  logout(): void {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.router.navigateByUrl('/auth/login', { replaceUrl: true });
+      },
+      error: () => {
+        this.alertService.displayMessage({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al cerrar sesión',
+        });
+      },
+    });
+  }
 }
