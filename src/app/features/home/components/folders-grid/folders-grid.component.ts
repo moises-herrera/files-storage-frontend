acab8e0b4c5a7716452329851412c126b25793e5
moises-herrera <moisesherrera037@gmail.com>
2025-03-27 09:18:43 -0500

    feat: get recent folders

--- src/app/features/home/components/folders-grid/folders-grid.component.ts
+++ src/app/features/home/components/folders-grid/folders-grid.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
 import { FolderRelated } from 'src/app/core/models/folder-related';
+import { AlertService } from 'src/app/core/services/alert.service';
+import { FolderService } from 'src/app/core/services/folder.service';
+import { TooltipModule } from 'primeng/tooltip';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-folders-grid',
-  imports: [],
+  imports: [CommonModule, RouterModule, TooltipModule],
   templateUrl: './folders-grid.component.html',
-  styleUrl: './folders-grid.component.css'
+  styleUrl: './folders-grid.component.css',
 })
 export class FoldersGridComponent {
-  folders: FolderRelated[] = [];
+  private readonly folderService = inject(FolderService);
+  private readonly alertService = inject(AlertService);
+
+  recentFolders$ = signal<Observable<FolderRelated[]>>(
+    this.folderService.getRecentFolders().pipe(
+      catchError(() => {
+        this.alertService.displayError(
+          'Error al cargar las carpetas recientes'
+        );
+        return of([]);
+      })
+    )
+  );
 }
