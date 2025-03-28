2ca2a5b5ba949eeffa0e9cea6a5690e14b74ac39
moises-herrera <moisesherrera037@gmail.com>
2025-03-28 11:10:41 -0500

    refactor: change folder tooltip

--- src/app/features/home/components/folders-grid/folders-grid.component.ts
+++ src/app/features/home/components/folders-grid/folders-grid.component.ts
@@ -4,12 +4,11 @@ import { Observable, catchError, of } from 'rxjs';
 import { FolderRelated } from 'src/app/core/models/folder-related';
 import { AlertService } from 'src/app/core/services/alert.service';
 import { FolderService } from 'src/app/core/services/folder.service';
-import { TooltipModule } from 'primeng/tooltip';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-folders-grid',
-  imports: [CommonModule, RouterModule, TooltipModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './folders-grid.component.html',
   styleUrl: './folders-grid.component.css',
 })
